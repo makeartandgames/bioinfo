ae9287d3c424bb68c92b91ecdc7ec7db32e3d173
makeartandgames <bigmwp@gmail.com>
2025-06-09 20:35:45 +0000

    remove redundant func

--- bioinfo/fasta.py
+++ bioinfo/fasta.py
@@ -15,14 +15,6 @@ from Bio.Seq import Seq
 
 print_function = print
 
-def make_seqrec(*args, **kwargs):
-    '''
-    Just call SeqRecord constructor, but wraps Seq object around string
-    for you
-
-    make_seqrec('ATGC', 'id', 'name', 'description')
-    '''
-    return SeqRecord(Seq(args[0]), *args[1:], **kwargs)
 
 # Maximum amount of permutations for ambiguous sequences that will
 # be generated
