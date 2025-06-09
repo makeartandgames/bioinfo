0b6de69e6a7ce1b8a414dd54d66ca513978b646c
makeartandgames <bigmwp@gmail.com>
2025-06-09 21:31:18 +0000

    Fix a number of stupid things

--- bioinfo/fasta.py
+++ bioinfo/fasta.py
@@ -1,7 +1,6 @@
 from __future__ import print_function
 import sys
 import argparse
-import string
 
 from Bio.SeqIO import parse
 from Bio.SeqIO import FastaIO
@@ -14,32 +13,7 @@ def parse_args():
         'fasta',
         help='Fasta file path or - to read from standard input'
     )
-    parser.add_argument(
-        '--wrap', default=False, action='store_const', const=80,
-        help='Default action. Converts all multi line sequences to single lines'
-    )
-    parser.add_argument(
-        '--split', action='store_true', default=False,
-        help='If set then split the input fasta on each identifier '
-            ' and create a new file for each identifier'
-    )
     return parser.parse_args()
-
-def writefasta(infile, outfile=sys.stdout, wrap=None):
-    fasta_out = FastaIO.FastaWriter(outfile, wrap=wrap)
-    fasta_out.write_file(parse(infile, 'fasta'))
-
-def splitfasta(infile, wrap=False):
-    for seq in parse(infile, 'fasta'):
-        outfile = str(seq.id)
-        for p in string.punctuation:
-            outfile = outfile.replace(p, '_')
-        outfile = outfile + '.fasta'
-        with open(outfile, 'w') as fh:
-            fasta_out = FastaIO.FastaWriter(fh, wrap=wrap)
-            fasta_out.write_header() # Does nothing, but required
-            fasta_out.write_record(seq)
-            fasta_out.write_footer() # Does nothing, but required
 
 def main():
     args = parse_args()
@@ -51,10 +25,9 @@ def main():
     else:
         _input = open(in_fasta)
 
-    if args.split:
-        splitfasta(_input, args.wrap)
-    else:
-        writefasta(_input, sys.stdout, wrap=args.wrap)
+    fasta_out = FastaIO.FastaWriter(sys.stdout, wrap=None)
+    fasta_out.write_file(parse(_input, 'fasta'))
+
 
 
 if __name__ == '__main__':
